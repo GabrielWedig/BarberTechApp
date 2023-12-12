9adf9394b2ba5a6d90570261f2e62f29aa471631
Gabriel Garcia Wedig <gabriel.wedig@cwi.com.br>
2023-12-12 15:57:06 -0300

    ✨ Title section

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from './header/Header'
 export * from './button/Button'
+export * from './container/Container'
--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,5 +1,19 @@
+//TODO: renomear o arquivo para constants
 export const colors = {
-  black: '#000000',
+  black: {
+    main: '#000000',
+    fifty: 'rgba(0, 0, 0, 0.50)',
+    eighty: 'rgba(0, 0, 0, 0.80)'
+  },
   white: '#FFFFFF',
-  orange: '#F0B35B'
+  orange: '#F0B35B',
+  blue: {
+    main: '#0D121E',
+    fifty: 'rgba(13, 18, 30, 0.50)',
+    ninety: 'rgba(13, 18, 30, 0.90)'
+  }
+}
+
+export const shadows = {
+  main: '3px 3px 8px'
 }
